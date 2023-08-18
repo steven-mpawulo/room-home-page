c3635554184a606823da8c73a79dad339fcd36f0
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-18 11:54:41 +0300

    finishes up on second section

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -4,35 +4,44 @@ import './main.css'
 import arrowLeft from '../../images/icon-arrow - Copy.svg'
 import angleLeft from '../../images/icon-angle-left - Copy.svg'
 import angleRight from '../../images/icon-angle-right - Copy.svg'
+import imageAboutDark from '../../images/image-about-dark.jpg'
+import imageAboutLight from '../../images/image-about-light.jpg'
 
 const Main = () => {
-  return (
-    <main>
-        <section className='first-section'>
-            <div className="first-section-left">
-                <Header/>
-            </div>
-            <div className="first-section-right">
-                <h1>Discover innovate <span>ways to decorate</span></h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et numquam animi eius eligendi accusantium, veniam esse ratione eos? Veniam repellendus deserunt ad sed quisquam laudantium eveniet doloribus fugiat id sequi!</p>
-                <div className="first-section-right-inner">
-                    <p>SHOP NOW</p>
-                    <img src={arrowLeft} alt="arrow-left" />
+    return (
+        <main>
+            <section className='first-section'>
+                <div className="first-section-left">
+                    <Header />
                 </div>
-                <div className="container-for-angle-arrows">
-                    <img src={angleLeft} alt="angle-left" />
-                    <img src={angleRight} alt="amgle-right" />
+                <div className="first-section-right">
+                    <h1>Discover innovate <span>ways to decorate</span></h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et numquam animi eius eligendi accusantium, veniam esse ratione eos? Veniam repellendus deserunt ad sed quisquam laudantium eveniet doloribus fugiat id sequi!</p>
+                    <div className="first-section-right-inner">
+                        <p>SHOP NOW</p>
+                        <img src={arrowLeft} alt="arrow-left" />
+                    </div>
+                    <div className="container-for-angle-arrows">
+                        <img src={angleLeft} alt="angle-left" />
+                        <img src={angleRight} alt="amgle-right" />
+                    </div>
                 </div>
-            </div>
-        </section>
-        <section className='second-section'>
-            <div className="second-section-first"></div>
-            <div className="second-section-second"></div>
-            <div className="second-section-third"></div>
+            </section>
+            <section className='second-section'>
+                <div className="second-section-first">
+                    <img src={imageAboutDark} alt="about-dark" />
+                </div>
+                <div className="second-section-second">
+                    <h2>ABOUT OUR FURNITURE</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis voluptatum dolorum magnam deleniti, totam ad quas quae, iste dolor molestiae consectetur itaque maxime ab labore! Consequuntur perferendis nesciunt praesentium cupiditate.</p>
+                </div>
+                <div className="second-section-third">
+                    <img src={imageAboutLight} alt="about-light" />
+                </div>
 
-        </section>
-    </main>
-  )
+            </section>
+        </main>
+    )
 }
 
 export default Main
