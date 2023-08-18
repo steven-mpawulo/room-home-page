d204baed21786f5659cfcb37d56fc24cda489f74
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-18 10:41:46 +0300

    adds nav element to header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,12 +7,12 @@ const Header = () => {
         <header>
             <img src={Logo} alt="logo" />
             <nav>
-            <ul>
-                <li><a href="#home">home</a></li>
-                <li><a href="#shop">shop</a></li>
-                <li><a href="#about">about</a></li>
-                <li><a href="#contact">contact</a></li>
-            </ul>
+                <ul>
+                    <li><a href="#home">home</a></li>
+                    <li><a href="#shop">shop</a></li>
+                    <li><a href="#about">about</a></li>
+                    <li><a href="#contact">contact</a></li>
+                </ul>
             </nav>
         </header>
     )
