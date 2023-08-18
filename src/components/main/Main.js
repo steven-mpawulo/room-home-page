9a47d69c30300458b02a4924a414d8f80371ec08
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-18 10:37:08 +0300

    adds header to first section

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Header from '../header/Header'
 
 const Main = () => {
   return (
     <main>
         <section className='first-section'>
-            <div className="first-section-left"></div>
+            <div className="first-section-left">
+                <Header/>
+            </div>
             <div className="first-section-right"></div>
         </section>
     </main>
